954c8e26657fb3744bf5604d6b81ad4948c9958f
YutaroDoi <doisaltu1899@gmail.com>
2019-04-13 19:28:56 +0900

    initial commit

---
+++ .zshrc
@@ -0,0 +1,1 @@
+/Users/doi/.zprezto/runcoms/zshrc
---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+Z_DOT_FILES=(.zshrc .zprofile .zshenv .zcompdump .zprezto .zcompdump.zwc .zlogin .zlogout .zpreztorc)
+
+# for file in ${Z_DOT_FILES[@]}
+#   do
+#     echo $file
+#     sed 's!^.*/!!' $file
+#   done
+
+# echo ${Z_DOT_FILES}
+
+
+
+for file in ${Z_DOT_FILES[@]}
+ do
+     ln -s $HOME/dotfiles/$file $HOME/$file
+ done
